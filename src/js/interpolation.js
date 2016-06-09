21ac00b1762323fa2174fdd47ca69ec56d1e294c
Tobi <fagerberg.tobias@gmail.com>
2016-06-09 22:52:22 +0200

    Add bounce interpolation

--- src/js/interpolation.js
+++ src/js/interpolation.js
@@ -9,3 +9,14 @@ export class Cosine {
 		return((1 - Math.cos(value * Math.PI)) / 2);
 	}
 }
+
+// Kudos: https://github.com/mootools/mootools-core/blob/master/Source/Fx/Fx.Transitions.js#L93
+export class Bounce {
+	interpolate(value) {
+		for (let a = 0, b = 1; 1; a += b, b /= 2){
+			if (value >= (7 - 4 * a) / 11) {
+				return b * b - Math.pow((11 - 6 * a - 11 * value) / 4, 2);
+			}
+		}
+	}
+}
