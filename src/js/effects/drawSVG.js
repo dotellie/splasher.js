3a272daadfd337416cb637d2b1adb3807645392c
Tobi <fagerberg.tobias@gmail.com>
2016-06-10 19:03:43 +0200

    Add fade to fill function in draw SVG

--- src/js/effects/drawSVG.js
+++ src/js/effects/drawSVG.js
@@ -9,6 +9,9 @@ export class DrawSVG extends SplashEffect {
 
 		this.interpolation = options.interpolation || new Linear();
 
+		this.fadeToFill = options.toFillFade;
+		this.fadeToFillDelay = options.toFillDelay || 0;
+
 		this.length = element.getTotalLength();
 		this.setStyle("stroke-dasharray", this.length);
 		this.setStyle("stroke-dashoffset", this.length);
@@ -16,6 +19,14 @@ export class DrawSVG extends SplashEffect {
 
 	in(value) {
 		this.setStyle("stroke-dashoffset", (value * -1 + 1) * this.length);
+
+		if (this.fadeToFill) {
+			if (value > this.fadeToFillDelay) {
+				let amount = (value - this.fadeToFillDelay) / (1 - this.fadeToFillDelay);
+				this.setStyle("fill-opacity", amount);
+				this.setStyle("stroke-opacity", amount * -1 + 1);
+			}
+		}
 	}
 
 	out(value) {
