9277c60a9f248949b3784f80b0aa62ba7bdf8546
Tobi <fagerberg.tobias@gmail.com>
2016-08-05 16:56:08 +0200

    Add units and different translation types to translate from position effect

--- src/js/effects/translateFromPosition.js
+++ src/js/effects/translateFromPosition.js
@@ -10,20 +10,38 @@ export class TranslateFromPosition extends SplashEffect {
 		this.x = options.x || 0;
 		this.y = options.y || 0;
 
+		// Can be whatever, but the effect won't do
+		// anything if it isn't a valid css unit.
+		this.unit = options.unit || "px";
+		// Can be either "transform" or "position"
+		this.translationType = options.translationType || "transform";
+
 		this.interpolation = options.interpolation || new Linear();
 	}
 
 	in(value) {
-		let x = this.interpolation.in(value * -1 + 1) * this.x,
-			y = this.interpolation.in(value * -1 + 1) * this.y;
-		this.setStyle("left", x + "px");
-		this.setStyle("top", y + "px");
+		this._set(
+			this.interpolation.in(value * -1 + 1) * this.x,
+			this.interpolation.in(value * -1 + 1) * this.y
+		);
 	}
 
 	out(value) {
-		let x = this.interpolation.out(value) * this.x,
-			y = this.interpolation.out(value) * this.y;
-		this.setStyle("left", x + "px");
-		this.setStyle("top", y + "px");
+		this._set(
+			this.interpolation.out(value) * this.x,
+			this.interpolation.out(value) * this.y
+		);
+	}
+
+	_set(x, y) {
+		if (this.translationType = "transform") {
+			this.setTransform("translateX", x + this.unit);
+			this.setTransform("translateY", y + this.unit);
+		} else if (this.translationType = "position") {
+			this.setStyle("left", x + this.unit);
+			this.setStyle("top", y + this.unit);
+		} else {
+			console.error("Unknown translation type: " + this.translationType);
+		}
 	}
 }
